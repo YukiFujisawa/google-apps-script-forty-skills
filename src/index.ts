794954eafb50de0bb4ad94869300eaaab07476af
yuki_fujisawa <g031y148yuki@gmail.com>
2023-11-19 11:51:03 +0900

    fix 中途半端にasideが入ってしまっていたので、asideを削除する

--- src/index.ts
+++
@@ -1,3 +0,0 @@
-import { hello } from './example-module';
-
-console.log(hello());
--- src/lesson1.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
-export function myFunction1() {
-  let x = 10;
-  console.log(x);
-  x = 20;
-  console.log(x);
-}
